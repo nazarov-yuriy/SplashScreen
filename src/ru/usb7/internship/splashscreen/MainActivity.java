86296be2e7e6def748e07ab0fb8d2b8f4c271971
Yuriy Nazarov <nazarov@chesapeake>
2014-06-20 20:25:40 +0400

    ListView added. Theme changed to theme without ActionBar.

--- src/ru/usb7/internship/splashscreen/MainActivity.java
+++ src/ru/usb7/internship/splashscreen/MainActivity.java
@@ -2,8 +2,8 @@ package ru.usb7.internship.splashscreen;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +11,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		String[] items = getResources().getStringArray(R.array.items);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, items);
+		ListView listview = (ListView) findViewById(R.id.listView1);
+		listview.setAdapter(adapter);
 	}
 }
